ae5d3e675840576cda18905ae3e3dfd22bc2123f
Juliya Smith <juliya.smith@code42.com>
2021-07-02 07:19:58 -0500

    *

--- jules42.py
+++ jules42.py
@@ -1,7 +1,9 @@
 import click
+import json
 
 from code42cli.extensions import script
 from code42cli.extensions import sdk_options
+from code42cli.cmds.auditlogs import _parse_audit_log_timestamp_string_to_timestamp  # Not public
 
 
 @click.group(name="jules")
@@ -31,10 +33,50 @@ def list_managers(state):
                 else:
                     managers[manager_username].append(username)
 
-    for manager in managers:
-        managed_employees_str = ", ".join(managers[manager])
-        output_str = f"{manager}: {managed_employees_str}"
-        click.echo(output_str)
+    json_text = _prettify_dict(managers)
+    click.echo(json_text)
+
+
+@main.command()
+@sdk_options
+def list_orgs(state):
+    """Lists the organizations."""
+    gen = state.sdk.orgs.get_all()
+    for response in gen:
+        org_list = response["orgs"]
+        for org in org_list:
+            data = json.dumps(org, indent=2)
+            click.echo(data)
+
+
+@main.command()
+@sdk_options
+@click.argument("org_id")
+def show_org(state, org_id):
+    """Show information about an Organization."""
+    org = state.sdk.orgs.get_by_uid(org_id)
+    data = json.dumps(org.data, indent=2)
+    click.echo(data)
+
+
+@main.command()
+@sdk_options
+def find_audit_log_date(state):
+    """Seek for audit log event timestamp formats that we don't handle correctly."""
+    gen = state.sdk.auditlogs.get_all()
+    for response in gen:
+        events = response["events"]
+        for event in events:
+            timestamp = event["timestamp"]
+            try:
+                _parse_audit_log_timestamp_string_to_timestamp(timestamp)
+            except ValueError:
+                click.echo("FOUND ONE!")
+                click.echo(event)
+
+
+def _prettify_dict(data):
+    return json.dumps(data, indent=2)
 
 
 if __name__ == "__main__":
